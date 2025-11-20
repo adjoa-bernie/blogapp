1a636cea1b6a48166abd6b836bde8e0a706d6cd9
bernie <adjoa.dev@gmail.com>
2025-11-20 11:35:57 +0000

    refactor(api): added prefixes to the routes

--- app/routers/post.py
+++ app/routers/post.py
@@ -2,15 +2,17 @@ from .. import models, schemas, database
 from sqlalchemy.orm import Session
 from fastapi import Response, status, HTTPException, Depends, APIRouter
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/posts",
+)
 
-@router.get("/posts", response_model=list[schemas.PostResponse])
+@router.get("", response_model=list[schemas.PostResponse])
 def get_posts(db: Session = Depends(database.get_db)):
     posts = db.query(models.Post).all()
     return posts
 
 
-@router.post('/posts', status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
+@router.post('', status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
 def create_post(post: schemas.Post, db: Session = Depends(database.get_db)):
     new_post = models.Post(**post.model_dump())
     db.add(new_post)
@@ -18,7 +20,7 @@ def create_post(post: schemas.Post, db: Session = Depends(database.get_db)):
     db.refresh(new_post)
     return new_post
 
-@router.get("/posts/{id}", response_model=schemas.PostResponse)
+@router.get("/{id}", response_model=schemas.PostResponse)
 def get_post(id:int,db: Session = Depends(database.get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first();
     if post:
@@ -27,7 +29,7 @@ def get_post(id:int,db: Session = Depends(database.get_db)):
         detail=f"post with id: {id} was not found")
 
 
-@router.put("/posts/{id}", response_model=schemas.PostResponse)
+@router.put("/{id}", response_model=schemas.PostResponse)
 def update_post(id:int, post_schema: schemas.PostUpdate, db: Session = Depends(database.get_db)):
     post = db.query(models.Post).filter(models.Post.id == id)
     post_to_update = post.first()
@@ -39,7 +41,7 @@ def update_post(id:int, post_schema: schemas.PostUpdate, db: Session = Depends(d
                         detail=f"post with id: {id} was not found")
 
 
-@router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id:int,db: Session = Depends(database.get_db)):
     deleted_post = db.query(models.Post).filter(models.Post.id == id).first()
     db.delete(deleted_post)
--- app/routers/user.py
+++ app/routers/user.py
@@ -2,15 +2,17 @@ from .. import models, schemas, database,utils
 from sqlalchemy.orm import Session
 from fastapi import Response, status, HTTPException, Depends, APIRouter
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/users",
+)
 
-@router.get("/users", response_model=list[schemas.UserResponse])
+@router.get("", response_model=list[schemas.UserResponse])
 def get_users(db: Session = Depends(database.get_db)):
     users = db.query(models.User).all()
     return users
 
 
-@router.get("/users/{id}", response_model=schemas.UserResponse)
+@router.get("/{id}", response_model=schemas.UserResponse)
 def get_user(id:int, db:Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if user:
@@ -18,7 +20,7 @@ def get_user(id:int, db:Session = Depends(database.get_db)):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
         detail=f"user with id: {id} was not found")
 
-@router.post('/users', status_code=status.HTTP_201_CREATED,response_model=schemas.UserResponse)
+@router.post('', status_code=status.HTTP_201_CREATED,response_model=schemas.UserResponse)
 def create_user(user: schemas.User, db: Session = Depends(database.get_db)):
     user.password = utils.hash(user.password)
     new_user = models.User(**user.model_dump())

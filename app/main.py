a81134973319f7ae98b69f30ed7e739f3c129934
bernie <adjoa.dev@gmail.com>
2025-11-20 09:49:18 +0000

    feat(api): added the endpoint to get all users and a single user

--- app/main.py
+++ app/main.py
@@ -68,3 +68,13 @@ def create_user(user: schemas.User, db: Session = Depends(database.get_db)):
     db.refresh(new_user)
     return new_user
 
+
+@app.get("/users", response_model=list[schemas.UserResponse])
+def get_users(db: Session = Depends(database.get_db)):
+    users = db.query(models.User).all()
+    return users
+
+@app.get("/users/{id}", response_model=schemas.UserResponse)
+def get_user(id:int, db:Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    return user
